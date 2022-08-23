0261bb825987fe59392cbe64d62a7d0d8b39dea8
Nehemiah <nehemiahdauda60@gmail.com>
2022-08-23 01:21:06 +0100

    modified all pages

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ $(function(){
 		$("#navbarCollapse").stop().animate({right:2*100 }, 'slow');
 	});
 });
+
+
+(function ($) {
+	var navbar = $('.navbar');
+	var lastScrollTop = 0;
+	
+	$(window).scroll(function () {
+		var st = $(this).scrollTop();
+		if (st > lastScrollTop) {
+			navbar.fadeOut();
+		}
+		else if(st < lastScrollTop && st > 200){
+			navbar.fadeIn();
+			navbar.removeClass('navbar-light bg-transparent').addClass('navbar-dark bg-custom');
+		}
+		else {
+			navbar.removeClass('navbar-dark bg-custom').addClass('navbar-light bg-transparent');
+		}
+		lastScrollTop = st;
+	});
+	
+})(jQuery);
+	
